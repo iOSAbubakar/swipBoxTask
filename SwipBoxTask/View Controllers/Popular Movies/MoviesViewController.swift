c5f2a425a5ab105ba7bf6ad67fa3065a6b3413dc
Abubakar <rawalrajpoot@gmail.com>
2024-09-23 15:37:51 -0700

    Initial commit

--- SwipBoxTask/View Controllers/Popular Movies/MoviesViewController.swift
+++ SwipBoxTask/View Controllers/Popular Movies/MoviesViewController.swift
@@ -18,7 +18,6 @@ class MoviesViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Do any additional setup after loading the view.
         setupTableView()
         
         if Reachability.isConnectedToNetwork(){
